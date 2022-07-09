b0e0ee909ab2fb444b570614f1ae46c70d7daed3
Tristan <tristanleighcole@googlemail.com>
2022-07-09 10:14:20 -0600

    Update omniauth.rb
    * Comment out previous trouble-shooting steps now that I've fixed the RubyMine errors that were stopping my code working (no method error, files not recognized - solution was to invalidate RM cache and restart)

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 # Required to allow get requests, which enables a security flaw but that's how the tutorial is set up.
-OmniAuth.config.allowed_request_methods = [:post, :get]
-OmniAuth.config.silence_get_warning = true
+# OmniAuth.config.allowed_request_methods = [:post, :get]
+# OmniAuth.config.silence_get_warning = true
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :developer if Rails.env.development?
